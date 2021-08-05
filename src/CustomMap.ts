35235ccf58ddffc06f3d920cd14c0f1f2d91678c
chrislemus <icrislemus@gmail.com>
2021-08-05 17:19:20 -0400

    add "addMarker" method to CustomMap class along with implicit type check

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,3 +1,9 @@
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -5,6 +11,16 @@ export class CustomMap {
     this.googleMap = new google.maps.Map(document.getElementById(divId), {
       zoom: 1,
       center: { lat: 0, lng: 0 },
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
     });
   }
 }
